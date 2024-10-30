74d6e170cb0de3f5dfde2b2a11ad9af7628321a7
Jess607 <ogwujessica2222@gmail.com>
2024-10-30 17:34:27 +0100

    Functions for outfit combinations with skirts

--- skirt_methods.py
+++ skirt_methods.py
@@ -8,9 +8,10 @@ import matplotlib.pyplot as plt
 import torchvision.models as models
 from preprocess import preprocess_image
 import torchvision.transforms as transforms
-from get_image_from_folder import get_images_from_drive
+from get_image_from_folder import get_images_from_folder
 from calculate_similarities import calculate_color_similarity, calculate_cosine_similarity, calculate_color_histogram
-
+from render_best_outfits import render_best_outfit_skirts
+from get_id import get_id
 
 # Load the pre-trained ResNet model with 18 layers
 resnet18 = models.resnet18(pretrained=True)
@@ -20,48 +21,54 @@ resnet18 = models.resnet18(pretrained=True)
 resnet18.eval()
 
 
-def get_id(best_matches, image_id):
-    final_lis=[]
-    for i in best_matches:
-            lis=[]
-            for j in range(len(i)-1):
-                  a=image_id[j][i[j]]
-                  lis.append(a)
-            final_lis.append(lis)
-    return final_lis
-
 
 
 def shirt_and_skirts_only(shirts_folder_id, skirts_folder_id):
+    #Store the data for the best matches in a list
     best_matches=[]
-    shirt_images, shirt_ids=get_images_from_drive(shirts_folder_id)
-    skirts_images, skirt_ids=get_images_from_drive(skirts_folder_id)
+    #Get images for the skirts and shirts
+    shirt_images, shirt_ids=get_images_from_folder(shirts_folder_id)
+    skirts_images, skirt_ids=get_images_from_folder(skirts_folder_id)
+    #Preprocess images and convert to tensors
     shirt_tensors=[preprocess_image(image) for image in shirt_images]
     skirt_tensors=[preprocess_image(image) for image in skirts_images]
+    #Extract features from tensors using resnet18
     with torch.no_grad():
         features_list1=[resnet18(input_tensor).squeeze().numpy() for input_tensor in shirt_tensors]
         features_list2=[resnet18(input_tensor).squeeze().numpy() for input_tensor in skirt_tensors]
+    #Iterate through every component of wardrobe and find similarities
     for i in range(len(shirt_images)):
+        #Get color histogram (H,S,V format) for shirt images
         hist1 = calculate_color_histogram(shirt_images[i])
         for j in range(len(skirts_images)):
+            #Get color histogram for skirts
             hist2 = calculate_color_histogram(skirts_images[j])
+            #Calculate cosine similarity between features
             feature_similarity = calculate_cosine_similarity(features_list1[i], features_list2[j])
+            #Calculate color similarity
             color_similarity  = calculate_color_similarity(hist1, hist2)
+            #Final similarity score is 50% of feature similarity and 50% of color similarity
             final_similarity_score = 0.5 * feature_similarity + 0.5 * color_similarity
+             #Append best matches
             best_matches.append([i,j,final_similarity_score])
     best_matches.sort(key=lambda x: x[2], reverse=True)
+    #Return best five outfits
     best_match_five=best_matches[:5]
+    #Get file id of images of outfits
     recommended_skirts=get_id(best_match_five, [shirt_ids, skirt_ids])
+    #Render the best outfit using matplotlib
+    render_best_outfit_skirts(skirts_images, skirt_ids, shirt_images, shirt_ids, recommended_skirts)
+    #Return ids of outfit combinations
     return recommended_skirts
 
 
 
 def shirts_and_skirts_with_bags_and_shoes(shirts_folder_id, skirts_folder_id, bags_folder_id, shoes_folder_id):
     best_matches=[]
-    shirt_images, shirt_ids=get_images_from_drive(shirts_folder_id)
-    skirts_images, skirts_ids=get_images_from_drive(skirts_folder_id)
-    bags_images, bags_ids=get_images_from_drive(bags_folder_id)
-    shoes_images, shoes_ids=get_images_from_drive(shoes_folder_id)
+    shirt_images, shirt_ids=get_images_from_folder(shirts_folder_id)
+    skirts_images, skirts_ids=get_images_from_folder(skirts_folder_id)
+    bags_images, bags_ids=get_images_from_folder(bags_folder_id)
+    shoes_images, shoes_ids=get_images_from_folder(shoes_folder_id)
     shirt_tensors=[preprocess_image(image) for image in shirt_images]
     trouser_tensors=[preprocess_image(image) for image in skirts_images]
     bag_tensors=[preprocess_image(image) for image in bags_images]
@@ -92,16 +99,17 @@ def shirts_and_skirts_with_bags_and_shoes(shirts_folder_id, skirts_folder_id, ba
                     best_matches.append([i,j,k,l,final_similarity_score])
     best_matches.sort(key=lambda x: x[4], reverse=True)
     best_match_five=best_matches[:5]
-    recommended_shirts=get_id(best_match_five, [shirt_ids, skirts_ids, shoes_ids, bags_ids])
-    return recommended_shirts
+    recommended_skirts=get_id(best_match_five, [shirt_ids, skirts_ids, bags_ids, shoes_ids])
+    render_best_outfit_skirts(skirts_images, skirts_ids, shirt_images, shirt_ids, recommended_skirts, bags_images, bags_ids, shoes_images, shoes_ids)
+    return recommended_skirts
 
 
 
 def shirts_and_skirts_with_bags(shirts_folder_id, skirts_folder_id, bags_folder_id):
     best_matches=[]
-    shirt_images, shirt_ids=get_images_from_drive(shirts_folder_id)
-    skirts_images, skirts_ids=get_images_from_drive(skirts_folder_id)
-    bags_images, bags_ids=get_images_from_drive(bags_folder_id)
+    shirt_images, shirt_ids=get_images_from_folder(shirts_folder_id)
+    skirts_images, skirts_ids=get_images_from_folder(skirts_folder_id)
+    bags_images, bags_ids=get_images_from_folder(bags_folder_id)
     shirt_tensors=[preprocess_image(image) for image in shirt_images]
     trouser_tensors=[preprocess_image(image) for image in skirts_images]
     bag_tensors=[preprocess_image(image) for image in bags_images]
@@ -125,17 +133,17 @@ def shirts_and_skirts_with_bags(shirts_folder_id, skirts_folder_id, bags_folder_
                 best_matches.append([i,j,k,final_score])
     best_matches.sort(key=lambda x: x[3], reverse=True)
     best_match_five=best_matches[:5]
-    recommended_shirts=get_id(best_match_five, [shirt_ids, skirts_ids, bags_ids])
-
-    return recommended_shirts
+    recommended_skirts=get_id(best_match_five, [shirt_ids, skirts_ids, bags_ids])
+    render_best_outfit_skirts(skirts_images, skirts_ids, shirt_images, shirt_ids, recommended_skirts, bags_images, bags_ids)    
+    return recommended_skirts
 
 
 
 def shirts_and_skirts_with_shoes(shirts_folder_id, skirts_folder_id, shoes_folder_id):
     best_matches=[]
-    shirt_images, shirt_ids=get_images_from_drive(shirts_folder_id)
-    skirts_images, skirts_ids=get_images_from_drive(skirts_folder_id)
-    shoes_images, shoes_ids=get_images_from_drive(shoes_folder_id)
+    shirt_images, shirt_ids=get_images_from_folder(shirts_folder_id)
+    skirts_images, skirts_ids=get_images_from_folder(skirts_folder_id)
+    shoes_images, shoes_ids=get_images_from_folder(shoes_folder_id)
     shirt_tensors=[preprocess_image(image) for image in shirt_images]
     trouser_tensors=[preprocess_image(image) for image in skirts_images]
     shoe_tensors=[preprocess_image(image) for image in shoes_images]
@@ -159,8 +167,9 @@ def shirts_and_skirts_with_shoes(shirts_folder_id, skirts_folder_id, shoes_folde
                 best_matches.append([i,j,k,final_score])
     best_matches.sort(key=lambda x: x[3], reverse=True)
     best_match_five=best_matches[:5]
-    recommended_shirts=get_id(best_match_five, [shirt_ids, skirts_ids, shoes_ids])
-    return recommended_shirts
+    recommended_skirts=get_id(best_match_five, [shirt_ids, skirts_ids, shoes_ids])
+    render_best_outfit_skirts(skirts_images, skirts_ids, shirt_images, shirt_ids, recommended_skirts, shoes_images, shoes_ids)
+    return recommended_skirts
 
 def get_skirt_recommendations(skirts=None, shirts=None, shoes=None, bags=None):
     skirt_list=[]
@@ -174,3 +183,8 @@ def get_skirt_recommendations(skirts=None, shirts=None, shoes=None, bags=None):
         skirt_list=shirt_and_skirts_only(shirts_folder_id=shirts, skirts_folder_id=skirts)
     return skirt_list
     
+
+#print(shirt_and_skirts_only('1betg3ShGBh7IWmdmrY5HFs0G7WAqjffK', '1lgStI-JQBItFBwI3Zgp1tt656S3EK9y2'))
+print(shirts_and_skirts_with_bags_and_shoes('1lgStI-JQBItFBwI3Zgp1tt656S3EK9y2','1betg3ShGBh7IWmdmrY5HFs0G7WAqjffK', '1i46TTlXXD5eSQPNMxkBb7QQgGdOJfOIM', '18EMeELFWs-IzVcwKe1PyMxMmRolz_86e' ))
+#print(shirts_and_skirts_with_bags('1betg3ShGBh7IWmdmrY5HFs0G7WAqjffK', '1lgStI-JQBItFBwI3Zgp1tt656S3EK9y2','1i46TTlXXD5eSQPNMxkBb7QQgGdOJfOIM'))
+print(shirts_and_skirts_with_shoes('1betg3ShGBh7IWmdmrY5HFs0G7WAqjffK', '1lgStI-JQBItFBwI3Zgp1tt656S3EK9y2','18EMeELFWs-IzVcwKe1PyMxMmRolz_86e'))
