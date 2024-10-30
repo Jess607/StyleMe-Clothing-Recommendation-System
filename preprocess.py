26dc7c9a42fb03636aaaf5ee8096815228902c58
Jess607 <ogwujessica2222@gmail.com>
2024-10-30 17:33:44 +0100

    Function to preprocess image

--- preprocess.py
+++ preprocess.py
@@ -5,16 +5,19 @@ import torchvision.transforms as transforms
 
 
 def preprocess_image(image):
-	# image = Image.open(image)
+	# Convert PIL image to NumPy array if needed
 	image_array=np.asarray(image)
+	# Define the transformation pipeline
 	transform = transforms.Compose([
 				transforms.ToPILImage(),
 				transforms.Resize((224, 224)),
 				transforms.ToTensor(),
 				transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 			])
+	# Apply transformations
 	input_tensor = transform(image_array)
-	input_tensor = input_tensor.unsqueeze(0)  # Add a batch dimension
+	# Add a batch dimension
+	input_tensor = input_tensor.unsqueeze(0)  
 	return input_tensor
 
 
