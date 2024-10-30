a884751e86eb42ac2de2ce87d8f3f9cfb91c3015
Jess607 <ogwujessica2222@gmail.com>
2024-10-30 17:28:52 +0100

    Flask API

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #import libraries
 from dress_methods import get_dress_recommendations
-from shirt_methods import get_shirts_recommendations 
+from pants_methods import get_shirts_recommendations 
 from skirt_methods import get_skirt_recommendations
 from flask import Flask, request, jsonify
 
